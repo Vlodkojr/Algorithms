e4ea1872ca51c8fbf0b711ea5848f3dee7ea8a15
Volodymyr Romanovskyi <volodymyrrom1@gmail.com>
2021-04-22 13:13:27 +0300

    Add noSwap

--- day3/sorting_bubble.js
+++ day3/sorting_bubble.js
@@ -1,17 +1,21 @@
+let noSwap;
 function swap (arr, indx1, indx2) {
     let temp = arr[indx1];
     arr[indx1] = arr[indx2];
     arr[indx2] = temp;
+    noSwap = false;
 }
 
 function bubble_sort (arr) {
     for(let i = arr.length; i > 0; i--) {
+        noSwap = true;
         for(let j = 0; j < i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
                swap(arr, j, j + 1);
             }
             // console.log(arr, arr[j], arr[j + 1]);
         }
+        if(noSwap) break;
     }
     console.log(arr);
     return arr;
