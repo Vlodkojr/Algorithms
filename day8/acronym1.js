b6d6abdf97729b2bfa313b0ee06fee58568ddf43
Volodymyr Romanovskyi <volodymyrrom1@gmail.com>
2021-05-17 23:49:38 +0300

    Add regexp to split

--- day8/acronym1.js
+++ day8/acronym1.js
@@ -1,10 +1,10 @@
 function acronym (str) {
     let arr = str
-    .split(' ')
+    .split(/[\s-]+/)
     .reduce((acr, word) => acr + word.match(/[A-Z]/i)[0], '')
     .toUpperCase();
     console.log(arr);
     return arr;
 }
 
-acronym("World Health Organization jhg");
+acronym("Something - I made up from thin air");
